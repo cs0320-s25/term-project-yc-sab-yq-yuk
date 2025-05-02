6d015ee707b848d412e46c48fbf2ce507217c38b
Yumian Cui <yumian_cui@brown.edu>
2025-05-02 18:21:16 -0500

    docs todos

--- server/src/main/java/com/brunomap/controller/TrendingController.java
+++ server/src/main/java/com/brunomap/controller/TrendingController.java
@@ -1,1 +1,39 @@
+package com.brunomap.controller;
 
+import com.brunomap.model.Event;
+import com.brunomap.service.TrendingService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/** Controller responsible for handling trending event logic */
+@RestController
+@RequestMapping("/api/trending")
+public class TrendingController {
+
+  @Autowired private TrendingService trendingService;
+
+  /**
+   * GET /api/trending Fetch a list of trending events, ranked by current trending scores
+   *
+   * @return list of trending events
+   */
+  @GetMapping
+  public List<Event> getTrendingEvents() {
+    // TODO
+    // return trendingService.getTrendingEvents();
+  }
+
+  /**
+   * POST /api/trending/recalculate Recalculate trending scores for all events triggered by a cron
+   * job or internal call
+   *
+   * @return success message after recalculation
+   */
+  @PostMapping("/recalculate")
+  public String recalculateTrendingScores() {
+    // TODO
+    // trendingService.recalculateTrendingScores();
+    // return "Trending scores recalculated successfully.";
+  }
+}
