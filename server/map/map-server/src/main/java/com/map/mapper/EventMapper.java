ac90fa428f38f8430e0e365d0b5a6b4a6b333910
Yuki-Zang <Matianyu_Zang@126.com>
2025-05-14 12:23:07 -0400

    edits

--- server/map/map-server/src/main/java/com/map/mapper/EventMapper.java
+++ server/map/map-server/src/main/java/com/map/mapper/EventMapper.java
@@ -9,7 +9,6 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
-import com.map.dataload.EventRecord;
 import com.map.entity.Event;
 
 /**
